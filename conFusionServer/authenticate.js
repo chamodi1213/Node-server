83d54c2092172abf4a7d220fe122bc2be8b360f2
unknown <madhushanichamodi@gmail.com>
2020-07-05 12:06:10 +0530

    Passport JWT

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -1,7 +1,38 @@
 var passport = require('passport');
 var LocalStragedy = require('passport-local').Strategy;
 var user = require('./models/user');
+var jwtStrategy = require('passport-jwt').Strategy;
+var extractJwt = require('passport-jwt').ExtractJwt;
+var jwt = require('jsonwebtoken'); // used to create, sign and verify tokens
+var config = require('./config');
 
 passport.use(new LocalStragedy(user.authenticate()));
 passport.serializeUser(user.serializeUser());
 passport.deserializeUser(user.deserializeUser());
+
+// create a webtoken
+exports.getToken = (user) => {
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+};
+
+var opts = {
+    jwtFromRequest : extractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey : config.secretKey
+};
+console.log("secret key:",opts.secretOrKey);
+// passport jwt strategy
+exports.jwtPassport = passport.use(new jwtStrategy(opts, (jwt_payload, done)=>{
+    console.log("JWT payload: ", jwt_payload);
+    user.findOne({_id: jwt_payload._id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        } else if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    });
+}));
+
+// verify using passport jwt
+exports.verifyUser = passport.authenticate('jwt', {session: false});
